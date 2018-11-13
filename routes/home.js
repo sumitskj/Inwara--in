a0ae06470e4114498c83194b3ae3e4a35a501442
sumitskj <sumitjangirdss.1@gmail.com>
2018-11-13 21:50:17 +0530

    added readme

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next)=>{
-    res.send(index);
+    res.send(index);//Serves the index.html file from dist folder
 });
 
 module.exports= router;
